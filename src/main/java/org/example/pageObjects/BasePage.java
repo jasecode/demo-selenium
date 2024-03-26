cfa4daf555008579d847c519ba2f24edca678863
IDK719\inspira <you@example.com>
2024-03-26 16:07:52 +0100

    test

---
+++ src/main/java/org/example/pageObjects/BasePage.java
@@ -0,0 +1,21 @@
+package org.example.pageObjects;
+
+
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class BasePage {
+    /*protected WebDriver driver;
+
+    public void setupDriver() {
+        // Set the path to the ChromeDriver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\inspira\\IdeaProjects\\UdemyCourseProject\\src\\main\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    public void closeBrowser() {
+        // Close the browser
+        driver.quit();
+    }*/
+}
